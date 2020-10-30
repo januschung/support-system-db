f0e0506a6542d4082b09dbfdd64441e8bfe4e91d
januschung <januschung@gmail.com>
2020-10-30 09:55:53 -0700

    update create_dbs.sql

--- docker/sql-scripts/create_dbs.sql
+++ docker/sql-scripts/create_dbs.sql
@@ -1,16 +1,17 @@
 DROP DATABASE IF EXISTS support_system;
 CREATE DATABASE support_system;
 
-CREATE USER 'jare'@'%' IDENTIFIED BY 'jare';
 CREATE USER 'jare'@'localhost' IDENTIFIED BY 'jare';
-CREATE USER 'liquibase'@'%' IDENTIFIED BY 'jare';
+CREATE USER 'jare'@'%' IDENTIFIED BY 'jare';
 CREATE USER 'liquibase'@'localhost' IDENTIFIED BY 'jare';
+CREATE USER 'liquibase'@'%' IDENTIFIED BY 'jare';
 
-GRANT SELECT,INSERT,UPDATE,EXECUTE ON support_system.* TO 'jare'@'%';
 GRANT SELECT,INSERT,UPDATE,EXECUTE ON support_system.* TO 'jare'@'localhost';
+GRANT SELECT,INSERT,UPDATE,EXECUTE ON support_system.* TO 'jare'@'%';
+GRANT ALL ON *.* to 'jare'@'%' IDENTIFIED BY 'jare';
 
-GRANT SUPER ON *.* TO 'liquibase'@'%';
 GRANT SUPER ON *.* TO 'liquibase'@'localhost';
+GRANT SUPER ON *.* TO 'liquibase'@'%';
 
-GRANT ALL ON support_system.* TO 'liquibase'@'%';
 GRANT ALL ON support_system.* TO 'liquibase'@'localhost';
+GRANT ALL ON support_system.* TO 'liquibase'@'%';
